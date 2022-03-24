72a52152f458ac5ecfb9a53b1323ecdda1bd17ec
Dmytro <dmitry.b@scopicsoftware.com>
2022-03-24 20:33:35 +0200

    tree

---
+++ src/main/java/com/learn/time/time/bases/generics/genericTree/Action1.java
@@ -0,0 +1,5 @@
+package com.learn.time.time.bases.generics.genericTree;
+
+public interface Action1<A> {
+    void apply(A arg1);
+}
---
+++ src/main/java/com/learn/time/time/bases/generics/genericTree/Func2.java
@@ -0,0 +1,5 @@
+package com.learn.time.time.bases.generics.genericTree;
+
+public interface Func2<A, B, R> {
+    R apply(A arg1, B arg2);
+}
