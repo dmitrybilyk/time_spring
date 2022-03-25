87d7fa2399da7e83f8564ced4e9b825af63cca91
Dmytro <dmitry.b@scopicsoftware.com>
2022-03-25 19:03:57 +0200

    collections

---
+++ src/main/java/com/learn/time/time/bases/collections/Collection/List/Vector/VectorMain.java
@@ -0,0 +1,20 @@
+package com.learn.time.time.bases.collections.Collection.List.Vector;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+/**
+ * Created by bid on 8/21/14.
+ */
+
+// Vector is the same as ArrayList but it's methods are synchronized
+public class VectorMain
+{
+   public static void main(String[] args)
+   {
+      List<String> vector = new Vector<String>();
+      vector.add("First");
+      System.out.println(vector.get(0));
+   }
+}
---
+++ src/main/java/com/learn/time/time/bases/collections/Map/TreeMap/TreeMapComparator.java
@@ -0,0 +1,10 @@
+package com.learn.time.time.bases.collections.Map.TreeMap;
+
+import java.util.Comparator;
+
+public class TreeMapComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2.compareTo(o1);
+    }
+}
