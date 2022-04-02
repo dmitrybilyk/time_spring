d1d8565a67fdb96d6cb6dfa006c62a59c0d8841f
Dmytro <dmitry.b@scopicsoftware.com>
2022-04-02 21:20:23 +0300

    spring boot

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+	id("org.springframework.boot") version "2.6.3"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("com.google.cloud.tools.jib") version "3.1.4"
+	id("com.avast.gradle.docker-compose") version "0.14.3"
+	id("org.unbroken-dome.helm") version "1.7.0" apply true
+	id("org.unbroken-dome.helm-releases") version "1.7.0" apply true
+	id("org.unbroken-dome.helm-publish") version "1.7.0" apply true
+	id("java")
+}
+
+repositories {
+	maven { url = uri("https://artifactory.zoomint.com/artifactory/zoom-ci-release") }
+	maven { url = uri("https://artifactory.zoomint.com/artifactory/ci-libs-release") }
+	maven { url = uri("https://artifactory.zoomint.com:80/artifactory/remote-repos") }
+	maven { url = uri("https://artifactory.zoomint.com/artifactory/zoom-dev-release") }
+	maven { url = uri("https://artifactory.zoomint.com/artifactory/zoom-dev-snapshot") }
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jersey")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.data:spring-data-jpa")
+	implementation("org.projectlombok:lombok")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	testImplementation("org.projectlombok:lombok")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("mysql:mysql-connector-java:8.0.19")
+	annotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test>() {
+	useJUnitPlatform()
+}
+
+dockerCompose {
+	useComposeFiles = listOf("docker-compose.yaml")
+}
+
+tasks.register("devRun") {
+	dependsOn(
+		tasks.getByPath("composeUp"),
+		tasks.getByPath("bootRun")
+	)
+	tasks.getByPath("bootRun").mustRunAfter(tasks.getByPath("composeUp"))
+}
